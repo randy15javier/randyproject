5925db8ddfa06a5aea98a8e2ddce22d35119f812
Randy <randyjavierh02@gmail.com>
2020-03-10 23:39:11 -0600

    Commit Inicial corregido 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send({ Hola: 'Todo bien, todo correcto y?' });
+    res.send({ Hola: 'Todo bien, todo correcto?' });
 });
 
 const PORT = process.env.PORT || 5000;
