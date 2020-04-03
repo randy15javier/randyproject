dd5ee7397f49708181c501a116cd358a81df7d1a
Randy <randyjavierh02@gmail.com>
2020-04-02 22:46:57 -0600

    Flujo Auth finalizado

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require("./config/keys");
+require('./models/Users');
+require('./services/passport');
+
+mongoose.connect(
+    keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(console.log('Conexión establecida con éxito'));
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({ Hola: 'Todo bien, todo correcto?' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https: //fathomless-garden-79886.herokuapp.com/
+// https: //git.heroku.com/fathomless-garden-79886.git
+// https: //git.heroku.com/fathomless-garden-79886.git
