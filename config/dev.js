403ecdd56c4fc68d45309958f86fc43266bb61d6
Randy <randyjavierh02@gmail.com>
2020-05-06 17:11:00 -0600

    All Working

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: "820934551243-j2b4t0868ppobhguothe8llf647gcvtp.apps.googleusercontent.com",
+  googleClientSecret: "fPXaRb4OZ0W1Pr1L_NDAU1Ri",
+  mongoURI: "mongodb+srv://admin123:admin123@cluster0-tkton.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "yaquierotenermititulo",
+  stripePublishableKey: "pk_test_Tp2NO02K2Mznew2Wv4FL0MUk00itsry6xK",
+  stripeSecretKey: "sk_test_Lr0l07Ik8yU47m6WfSWSAJ6800faLKviA0",
+};
